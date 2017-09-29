06ff8bfe0edb31cc28dfc978fad22eddcffe15d4
toLoveCS <30851744+toLoveCS@users.noreply.github.com>
2017-09-30 00:05:44 +0800

    Add files via upload
    improve 414 pro by using Integer,the time complexity is o(n),the space complexity is o(1)

--- 414.ThirdMaximumNumber.java
+++ 414.ThirdMaximumNumber.java
@@ -37,3 +37,39 @@ class Solution {
 
     }
 }
+
+//时间复杂度o(n)
+//空间复杂度o(1)
+//learn by discuss
+class Solution {
+    public int thirdMax(int[] nums) {
+
+        Integer thirdMaxNum = null;
+        Integer secondMaxNum = null;
+        Integer firstMaxNum = null;
+
+        for(Integer n : nums)
+        {
+        	if(n.equals(firstMaxNum) || n.equals(secondMaxNum) || n.equals(thirdMaxNum)) continue;
+        	if(firstMaxNum == null || n > firstMaxNum) 
+        	{
+        		thirdMaxNum = secondMaxNum;
+        		secondMaxNum = firstMaxNum;
+        		firstMaxNum = n;
+        	}
+        	else if(secondMaxNum == null || n > secondMaxNum)
+        	{
+        		thirdMaxNum = secondMaxNum;
+        		secondMaxNum = n;
+        	}
+        	else if(thirdMaxNum == null || n > thirdMaxNum)
+        	{
+        		thirdMaxNum = n;
+        	}
+        }
+        
+
+        return thirdMaxNum == null ? firstMaxNum : thirdMaxNum;
+
+    }
+}
