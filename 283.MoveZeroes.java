1d28cdf1ba3f8755b294e9fafb57dd491fd9126e
toLoveCS <30851744+toLoveCS@users.noreply.github.com>
2017-10-02 23:41:00 +0800

    Add files via upload

---
+++ 283.MoveZeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+    	int countzero = 0;
+    	for(int i = 0 ; i < nums.length ; i++)
+    	{
+    		if(nums[i] == 0) countzero++;
+    	}
+     	
+
+
+
+    }
+}
