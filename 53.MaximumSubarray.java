0e4c736669edaf49a4768c75dea07cfd0b538b37
toLoveCS <30851744+toLoveCS@users.noreply.github.com>
2017-10-12 19:50:50 +0800

    Add files via upload

--- 53.MaximumSubarray.java
+++ 53.MaximumSubarray.java
@@ -1,9 +1,25 @@
+//DP 2017/10/12
+//b[j]=max{b[j-1]+a[j],a[j]}
+//time o(n)
+//space o(1)
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sum = 0;
-        for(int i = 0; i < nums.length; i++)
+        int sum = nums[0] ,b = nums[0];
+        for(int i = 1; i < nums.length; i++)
         {
-        	if(sum + nums[i] < 0)
-        }
+          if(b < 0) 
+            b = nums[i];
+          else
+            b += nums[i];
+          if(b > sum)
+            sum = b;
+         }
+
+        return sum;
     }
 }
+
+//divide and conquer 2017/10/ 
+//time o()
+//space o()
+
